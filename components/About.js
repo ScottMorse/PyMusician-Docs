45483b814c606d886f197902779c724b22cc0927
Scott Morse <scottmorsedev@gmail.com>
2019-01-27 13:00:36 -0600

    Making about page

--- components/About.js
+++ components/About.js
@@ -1,8 +1,18 @@
 import styled from 'styled-components'
 
 const StyledAbout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1{
+    font-family: ${props => props.theme.display};
+    font-size: 3em;
+    color: white;
+    text-shadow: 0.04em 0.04em ${props => props.theme.darkPurple};
+  }
 `
 
 export default () => <StyledAbout>
-  
+  <h1>About</h1>
+  <p></p>
 </StyledAbout>
