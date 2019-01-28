ff0f553aefb50375b896b2e6e61b072f7a5e41c6
Scott Morse <scottmorsedev@gmail.com>
2019-01-28 17:04:02 -0600

    May have finally found workaround to next-page-transition error

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
 import Home from '../components/Home'
 
-// export default (props) => <Home/>
+export default (props) => <Home/>
 
-export default class Index extends Component {
-  static pageTransitionReadyToEnter = true
+// export default class Index extends Component {
+//   static pageTransitionReadyToEnter = true
 
-  componentDidMount(){
-    this.props.pageTransitionReadyToEnter()
-  }
+//   componentDidMount(){
+//     this.props.pageTransitionReadyToEnter()
+//   }
 
-  render(){
-    return <Home/>
-  }
-}
+//   render(){
+//     return <Home/>
+//   }
+// }
